3a955d94bc1407e3844a8de599b3c878199598a6
Jason <jajones414@gmail.com>
2022-11-12 14:58:06 -0600

    I havee added get, post, and delete routes to the server.js file.

--- server.js
+++ server.js
@@ -16,23 +16,45 @@ app.use(express.static("./public"));
 
 
 // get
-app.get("/api/notes", function (req, res) {
-    
-    res.json(notes);
+app.get("/api/notes", function(req, res) {
+    readFile("./db/db.json", "utf8").then(function(data) {
+        notes = [].concat(JSON.parse(data))
+        res.json(notes);
+    })
 });
 
 
 // post
-app.post("/api/notes", function (req, res) {
-
-    res.json(note);
+app.post("/api/notes", function(req, res) {
+    const note = req.body;
+    readFile("./db/db.json", "utf8").then(function(data) {
+        const notes = [].concat(JSON.parse(data));
+        note.id = notes.length + 1
+        notes.push(note)
+        return notes
+    }).then(function(notes) {
+        writeFile("./db/db.json", JSON.stringify(notes))
+        res.json(note);
+    })
 });
 
 
 // delete
-app.delete("/api/note/:id", function (req, res) {
-
-    res.send();
+app.delete("/api/note/:id", function(req, res) {
+    const deleteId = parseInt(req.params.id);
+    readFile("./db/db.json", "utf8").then(function(data) {
+        const notes = [].concat(JSON.parse(data));
+        const newNotes = []
+        for (let i = 0; i < notes.length; i++) {
+            if(deleteId !== notes[i].id) {
+                newNotes.push(notes[i])
+            }
+        }
+        return newNotes
+    }).then(function(notes) {
+        writeFile("./db/db.json", JSON.stringify(notes))
+        res.send("Note saved.");
+    })
 });
 
 
