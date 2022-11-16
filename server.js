8759ddc7ae7ccdedfabac3bfde3039c52c63690b
Jason <jajones414@gmail.com>
2022-11-15 18:09:58 -0600

    I have worked with a tutor and corrected route erros as well as workspace folder path errors and now the application gets, posts, and DELETES!

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.static("./public"));
 
 // get
 app.get("/api/notes", (req, res) => {
-    fs.readFile(path.join(__dirname, "db", "db.json"), "utf-8", function(err, data) {
+    fs.readFile(path.join(__dirname, "./db/db.json"), "utf-8", function(err, data) {
         res.json(JSON.parse(data));
     })
 });
@@ -31,16 +31,16 @@ app.post("/api/notes", (req, res) => {
     }
     const nextNote = {
         ...req.body,
-    id:Math.random()
+    id:Math.floor(Math.random() * 2000)
     };
-    fs.readFile(path.join(__dirname, "db", "db.json"), "utf-8", function(err, data) {
+    fs.readFile(path.join(__dirname, "./db/db.json"), "utf-8", function(err, data) {
         if (err) {
             res.status(500).json(err)
             return
         }
         const noteData = JSON.parse(data)
         noteData.push(nextNote)
-        fs.writeFile(path.join(__dirname, "db", "db.json"), JSON.stringify(noteData), function(err) {
+        fs.writeFile(path.join(__dirname, "./db/db.json"), JSON.stringify(noteData), function(err) {
             if (err) {
                 res.status(500).json(err);
                 return
@@ -54,7 +54,8 @@ app.post("/api/notes", (req, res) => {
 // delete
 app.delete("/api/notes/:id", (req, res) => {
     const deleteId = parseInt(req.params.id);
-    fs.readFiles(path.join(__dirname, "db", "db.json"), "utf-8").then((data) =>{
+    console.log(deleteId)
+    fs.readFile(path.join(__dirname, "./db/db.json"), "utf-8", function(err, data) {
         const notes = [].concat(JSON.parse(data));
         const keepNotes = []
         for (let i = 0; i < notes.length; i++) {
@@ -62,25 +63,26 @@ app.delete("/api/notes/:id", (req, res) => {
                 keepNotes.push(notes[i]);
             }
         }
-        return newNotes
-    }).then((notes) => {
-        fs.writeFiles("./db/db.json", JSON.stringify(notes));
-        res.send("Note saved.");
+        console.log(keepNotes)
+        fs.writeFile("./db/db.json", JSON.stringify(keepNotes), function(err, data){
+
+            res.send("Note saved.");
+        });
     })
 });
 
 
 // html
 app.get("/notes", (req, res) => {
-    res.sendFile(path.join(__dirname, "public", "notes.html"));
+    res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "public", "index.html"));
+    res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "public", "index.html"));
+    res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
 // listen
